675aca93f953ded4676a050f3afba2698b695b1d
Franz Seidel <franz8818@gmail.com>
2022-12-26 23:29:32 +0000

    avance de todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,54 @@
-import React, {useState} from "react"; // 1. IMPORTO EL HOOK useState
+import React, { useState } from "react"; // 1. IMPORTO EL HOOK useState
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [email,setEmail]=useState("") // 2. EJECUTO EL HOOK useState
-	const [password,setPassword]=useState("")
+  function todoList() {}
+  
+  const [todos, setTodos] = useState(data); // 2. EJECUTO EL HOOK useState
 
-	function handleEmail() {
-
-	}
-	
-	return (
-		<div className="text-center">
-         <h1 className="text-center mt-5">Trabajando con componentes</h1>
-		 <form className="container">
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tu correo aquí" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Tu contraseña aqui" />
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-
-		</div> 
-	);
+  console.log(todos);
+   return (
+    <div className="container col-md-4">
+      <div class="col-md-18">
+      <h1>TODOS</h1>  
+      <div className="input-group flex-nowrap">
+        <span className="input-group-text" id="addon-wrapping">
+          ¡Para hacer!
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="addon-wrapping"
+        />
+       </div>
+       <ol class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">{todos}</div>
+    </div>
+    <span class="badge bg-primary rounded-pill">X</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Trabajar</div>
+    </div>
+    <span class="badge bg-primary rounded-pill">X</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Trotar</div>
+    </div>
+    <span class="badge bg-primary rounded-pill">X</span>
+  </li>
+</ol>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
